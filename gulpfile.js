7528be2f404c7547d436275e0dc3b75bb48e3f18
Aleksej Kamynin <zendzirou@gmail.com>
2014-12-06 19:36:30 +0300

    Добавил внешние зависимости

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,31 @@
 var gulp = require('gulp');
 var livereload = require('gulp-livereload');
+var concat = require('gulp-concat');
+var vendor = {
+  css: [
+    'src/_vendor/normalize.css/normalize.css',
+  ],
+  js: [
+    'src/_vendor/console-polyfill/index.js',
+    'src/_vendor/location.origin.js'
+  ]
+};
 
 gulp.task('watch', function() {
   livereload.listen();
-  gulp.watch(['src/**']).on('change', livereload.changed);
+  gulp.watch(['src/build/**', 'src/**.jade', 'src/javascripts/**']).on('change', livereload.changed);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('vendor-css', function() {
+  return gulp.src(vendor.css)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('src/build/'));
+});
+
+gulp.task('vendor-js', function() {
+  return gulp.src(vendor.js)
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest('src/build/'));
+});
+
+gulp.task('default', ['watch', 'vendor-css', 'vendor-js']);
